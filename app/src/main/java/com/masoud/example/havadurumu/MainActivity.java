097db61150cdbfe2b3ad5cd6624654979c532b95
mesut6283 <masoud_m_tr@yahoo.com>
2016-03-14 12:27:14 +0200

    ezafe kardane fragment be mainactivity va nemyeshe an

--- app/src/main/java/com/masoud/example/havadurumu/MainActivity.java
+++ app/src/main/java/com/masoud/example/havadurumu/MainActivity.java
@@ -1,5 +1,7 @@
 package com.masoud.example.havadurumu;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        MainActivityFragment fragment=new MainActivityFragment();
+        fragmentTransaction.add(R.id.fragment_container,fragment);
+        fragmentTransaction.commit();
+
     }
 }
